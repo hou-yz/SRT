3359af2f2bfc3c7947e89d8cb7eef71b71ba72e6
hou-yz <hou_yz@outlook.com>
2017-08-26 12:12:07 +0800

    Add files via upload

---
+++ SRT/srt_framework.m
@@ -0,0 +1,16 @@
+clear
+clc
+T_range=60;%an hour
+delta_t=60;
+J_range=3;
+N=1;
+P_i_max=10;P_j_max=1;
+BW=20*10^6;%bandwidth=20MHz
+%pos_i=zeros(2,1);pos_j=ones(2,t_range,J_range);
+pos_j=srt_pos_j(J_range,T_range);%rand(J_range,T_range)+1i*rand(J_range,T_range);
+C_qos=10^7*ones(1,J_range);
+pos_i=[zeros(1,T_range);pos_j];
+r=zeros(J_range+1,J_range,T_range);
+beta=srt_beta(pos_i,pos_j);
+gamma=0*ones(J_range,J_range);
+srt_algorithm(beta,J_range,T_range,P_i_max,P_j_max,C_qos,N,gamma,delta_t,BW)
