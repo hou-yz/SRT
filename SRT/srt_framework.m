6b9f05b8ceb77489b3893e94891b343923dc2de8
hou-yz <hou_yz@outlook.com>
2017-09-02 15:36:53 +0800

    Add files via upload

--- SRT/srt_framework.m
+++ SRT/srt_framework.m
@@ -64,49 +64,51 @@ gamma=0*ones(J_range,J_range);
 % save('.\experiments\delta_ts_15_240','delta_ts','srt_E_means','ref_E_means')
 % plot(delta_ts,srt_E_means(1,:),'r',delta_ts,srt_E_means(2,:),'r--o',delta_ts,ref_E_means(1,:),'g');
 
-% %T_range 变化
-% divs=[1,2,3,4,5,6,12,30,60];
-% srt_Es=zeros(2,length(divs));ref_Es=zeros(2,length(divs));
-% srt_unsatisfied=zeros(1,length(divs));ref_unsatisfied=zeros(1,length(divs));
-% [ref_E,C_unsatisfied]=ref_algorithm(beta,J_range,T_range,P_i_max,P_j_max,C_qos,N,gamma,delta_t,BW);
-% for i=(1:length(divs))
-%     div=divs(i);
-%     C_unsatisfied=C_qos;
-%     fprintf('T_range_div=%d\n',div)
-%     T_range_partial=T_range/div;
-%     ii=1;
-%     while ii<=div && sum(C_unsatisfied)
-%         pos_j_partial=pos_j(:,T_range_partial*(ii-1)+1 : T_range_partial*ii);
-%         pos_i_partial=pos_i(:,T_range_partial*(ii-1)+1 : T_range_partial*ii);
-%         beta_partial=beta(:,:,T_range_partial*(ii-1)+1 : T_range_partial*ii);
-%         gamma=0*ones(J_range,J_range);
-%         srt_Es_partial=zeros(2,1);
-%         [srt_Es_partial(1),srt_Es_partial(2),C_unsatisfied]=srt_algorithm(beta_partial,J_range,T_range_partial,P_i_max,P_j_max,C_unsatisfied,N,gamma,delta_t,BW);
-%         srt_Es(:,i)=srt_Es(:,i)+srt_Es_partial;
-%         ii=ii+1;
-%     end
-% end
-% 
-% srt_Es_means=[srt_Es(1,:)./J_range;srt_Es(2,:)./J_range];
-% ref_Es_means=ref_E*ones(1,length(divs))./J_range;
-% 
-% save('.\experiments\T_ranges_1_60','divs','srt_Es_means','ref_Es_means')
-% plot(divs,srt_Es_means(1,:),'r',divs,srt_Es_means(2,:),'r--o',divs,ref_Es_means,'g');
-
-%C_qos 变化
-multis=[0.1,0.2,0.5,0.8,1,1.2,1.5,2,5];
-srt_Es=zeros(2,length(multis));ref_Es=zeros(2,length(multis));
-for i=(1:length(multis))
-    multi=multis(i);
-    C_qos_new=C_qos*multi;
-    fprintf('C_qos_new=%d\n',C_qos_new(1))
-    [srt_Es(1,i),srt_Es(2,i)]=srt_algorithm(beta,J_range,T_range,P_i_max,P_j_max,C_qos_new,N,gamma,delta_t,BW);
-    [ref_Es(1,i),ref_Es(2,i)]=ref_algorithm(beta,J_range,T_range,P_i_max,P_j_max,C_qos_new,N,gamma,delta_t,BW);
+%T_range 变化
+divs=[1,2,3,4,5,6,12,30,60,120,240];
+srt_Es=zeros(2,length(divs));ref_Es=zeros(1,length(divs));
+srt_unsatisfied=zeros(1,length(divs));ref_unsatisfied=zeros(1,length(divs));
+[ref_E,C_unsatisfied]=ref_algorithm(beta,J_range,T_range,P_i_max,P_j_max,C_qos,N,gamma,delta_t,BW);
+for i=(1:length(divs))
+    div=divs(i);
+    C_unsatisfied=C_qos;
+    fprintf('div=%d\n',div)
+    T_range_partial=T_range/div;
+    ii=1;
+    while ii<=div && sum(C_unsatisfied)
+        pos_j_partial=pos_j(:,T_range_partial*(ii-1)+1 : T_range_partial*ii);
+        pos_i_partial=pos_i(:,T_range_partial*(ii-1)+1 : T_range_partial*ii);
+        beta_partial=beta(:,:,T_range_partial*(ii-1)+1 : T_range_partial*ii);
+        gamma=0*ones(J_range,J_range);
+        srt_Es_partial=zeros(2,1);
+        [srt_Es_partial(1),srt_Es_partial(2),C_unsatisfied]=srt_algorithm(beta_partial,J_range,T_range_partial,P_i_max,P_j_max,C_unsatisfied,N,gamma,delta_t,BW);
+        srt_Es(:,i)=srt_Es(:,i)+srt_Es_partial;
+        ii=ii+1;
+    end
 end
 
-srt_Es_means=[srt_Es(1,:)./J_range./(multis);srt_Es(2,:)./J_range./(multis)];
-ref_Es_means=[ref_Es(1,:)./J_range./(multis);ref_Es(2,:)./J_range./(multis)];
+srt_Es_means=[srt_Es(1,:)./J_range;srt_Es(2,:)./J_range];
+ref_Es_means=ref_E*ones(1,length(divs))./J_range;
 
-save('.\experiments\C_qos_0.1_5.mat','multis','srt_Es_means','ref_Es_means')
-plot(multis,srt_Es_means(1,:),'r',multis,srt_Es_means(2,:),'r--o',multis,ref_Essss_means(1,:),'g',multis,ref_Essss_means(2,:),'g--o');
+save('.\experiments\T_ranges_1_240','divs','srt_Es_means','ref_Es_means')
+plot(divs,srt_Es_means(1,:),'r',divs,srt_Es_means(2,:),'r--o',divs,ref_Es_means,'g');
 
+% %C_qos 变化
+% multis=[0.1,0.2,0.5,0.8,1,1.2,1.5,2,3,4,5,6,7,8,9,10];
+% srt_Es=zeros(2,length(multis));ref_Es=zeros(1,length(multis));
+% srt_unsatisfied=zeros(1,length(multis));ref_unsatisfied=zeros(1,length(multis));
+% for i=(1:length(multis))
+%     multi=multis(i);
+%     C_qos_new=C_qos*multi;
+%     fprintf('multi=%2.2f\n',multi)
+%     [srt_Es(1,i),srt_Es(2,i),C_unsatisfied]=srt_algorithm(beta,J_range,T_range,P_i_max,P_j_max,C_qos_new,N,gamma,delta_t,BW);
+%     srt_unsatisfied(1,i)=(sum(C_unsatisfied)>0);
+%     [ref_Es(1,i),C_unsatisfied]=ref_algorithm(beta,J_range,T_range,P_i_max,P_j_max,C_qos_new,N,gamma,delta_t,BW);
+%     ref_unsatisfied(1,i)=(sum(C_unsatisfied)>0);
+% end
+% 
+% srt_Es_means=[srt_Es(1,:)./J_range./(multis);srt_Es(2,:)./J_range./(multis)];
+% ref_Es_means=ref_Es./J_range./(multis);
+% 
+% save('.\experiments\C_qos_0.1_10.mat','multis','srt_Es_means','ref_Es_means','srt_unsatisfied','srt_unsatisfied')
+% plot(multis,srt_Es_means(1,:).*(1-srt_unsatisfied),'r',multis,srt_Es_means(2,:).*(1-srt_unsatisfied),'r--o',multis,ref_Es_means.*(1-ref_unsatisfied),'g');
