9cae0988f1df422ed0e0dd078c1b0926c62dfdd2
hou-yz <hou_yz@outlook.com>
2017-09-07 19:48:00 +0800

    remove files in ./SRT/

--- SRT/srt_channel.m
+++
@@ -1,6 +0,0 @@
-function r = srt_channel(P,BW,beta)
-h0=1;sigma=10^-4;
-[I_range,J_range,T_range]=size(beta);
-P=reshape(P*ones(1,J_range*T_range),I_range,J_range,T_range);
-r=BW*log(1+P.*beta*h0^2/sigma^2)/log(2);
-end
--- SRT/srt_get_p.m
+++
@@ -1,7 +0,0 @@
-function [ P ] = srt_get_p( r0,BW,beta_this )
-%SRT_GET_P Summary of this function goes here
-%   Detailed explanation goes here
-h0=1;sigma=10^-4;
-P=(2^(r0/BW)-1)*sigma^2/(beta_this*h0^2);
-end
-
