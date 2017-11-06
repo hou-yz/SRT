dd95d0660fc98dbc0475cb0677f20e230f396b97
hou-yz <hou_yz@outlook.com>
2017-11-06 19:46:21 +0800

    update for us-based algorithm & latex
    虽然US来解决EE不合理，但是鉴于时间因素，只能这样了

--- matlab/ref_algorithm.m
+++ matlab/ref_algorithm.m
@@ -2,25 +2,25 @@ function [E_stage_1,C_unsatisfied]=ref_algorithm(beta,J_range,T_range,P_i_max,P_
 % service begin time -- bt
 % service end time   -- et
 P=reshape([P_i_max;ones(J_range,1)*P_j_max]*ones(1,J_range*T_range),J_range+1,J_range,T_range);
+P_s=linspace(0,P_i_max,101);
+P_i_tmp=ones(J_range+1,J_range,T_range,1).*reshape(P_s,[1,1,1,101]);
+%fast fading, size(h0,3)>T_range
 T_range_h0=size(h0,3);
 t_step=T_range_h0/T_range;
-r=zeros(J_range+1,J_range,T_range);
+r=zeros(J_range+1,J_range,T_range,101);
 F_x= @(x,z) exp(-z.*x)./x;
-if t_step>=1
+if t_step>1
     for t=(1:T_range)
         t_h0=(t-1)*t_step;
-        r(:,:,t)=sum(BW*log(1+P(:,:,t).*ones(1,1,t_step).*(beta(:,:,t).*ones(1,1,t_step)).*abs(h0(:,:,t_h0+1:t_h0+t_step)).^2/sigma2)/log(2),3);
+        r(:,:,t,:)=sum(BW*log2(1+P_i_tmp(:,:,t,:).*(beta(:,:,t).*ones(1,1,t_step,101)).*(abs(h0(:,:,t_h0+1:t_h0+t_step)).^2.*ones(1,1,1,101))/sigma2),3);
     end
     r=r/t_step;
-%     r_g=r;
 else
-    gamma=beta.*P./sigma2;
+    gamma=beta.*ones(1,1,1,101).*P_i_tmp./sigma2;
     F_int=integral(@(x) F_x(x,1./gamma),1,inf,'ArrayValued',true);
     r=BW*log2(exp(1)).*exp(1./gamma).*F_int;
     r(isnan(r))=0;r(r==inf)=0;
 end
-
-% r=BW*log(1+P.*beta.*h0.^2/sigma2)/log(2);
 
 
 S=[];%[i,j,t]->[0,j,t,0,0,0]
@@ -29,61 +29,52 @@ C_max=C_qos;
 eta=zeros(J_range+1,J_range,T_range);
 C_tmp=zeros(J_range,T_range+1);
 %C_tmp(j,t) means have that much data at the time slot before t
-unsatisfied_j=(1:J_range);
+satisfied=zeros(1,J_range);
+j=1;
 for t0=(1:T_range) %遍历时隙
-    if isempty(unsatisfied_j)
+    j_add=0;
+    if prod(satisfied)
         break
     end
-    [~,beta_sorted_j_pointer]=sort(r(1,unsatisfied_j,t0),'descend');
-    ijt_j_pointer=1;
-    j=unsatisfied_j(beta_sorted_j_pointer(ijt_j_pointer));
-    satisfied_cnt=0;
-    
-    while sum(C_tmp(:,T_range+1)<C_qos(:)) && sum(eta(1,:,t0)>0)<N && sum(r(0+1,unsatisfied_j,t0)) %若不满足qos条件，增加一条边  
-        if t0<bt(j) || t0>et(j)
-            ijt_j_pointer=ijt_j_pointer+1;
-            if ijt_j_pointer-satisfied_cnt>length(unsatisfied_j)
-                break
+    while sum(C_tmp(:,T_range+1)<C_qos(:)) && sum(eta(1,:,t0)>0)<N && j_add<J_range %若不满足qos条件，增加一条边  
+        if t0<bt(j) || t0>et(j) || satisfied(j)
+            j=j+1;
+            j_add=j_add+1;
+            
+            if j>J_range
+                j=j-J_range;
             end
-            j=unsatisfied_j(beta_sorted_j_pointer(ijt_j_pointer-satisfied_cnt));
             continue
         end
-        
-        [~,beta_sorted_j_pointer]=sort(r(1,unsatisfied_j,t0),'descend');
-        if r(0+1,j,t0)==0
-            %fprintf('srt:用户%d未能满足QoS限制',j)
-            break
-        end
-%         edge1=[0,j,t0]
-        if C_tmp(j,T_range+1)+r(0+1,j,t0)*delta_t<C_qos(j)%未溢出
+        if C_tmp(j,T_range+1)+r(0+1,j,t0,101)*delta_t<C_qos(j)%未溢出
             eta(0+1,j,t0)=1;%index 0 for BS-i
-            C_tmp(j,t0:T_range+1)=C_tmp(j,t0:T_range+1)+r(0+1,j,t0)*delta_t*eta(0+1,j,t0);
+            C_tmp(j,t0:T_range+1)=C_tmp(j,t0:T_range+1)+r(0+1,j,t0,101)*delta_t;
         else%溢出
-            eta(0+1,j,t0)=(C_qos(j)-C_tmp(j,T_range+1))/(r(0+1,j,t0)*delta_t);
-            C_tmp(j,t0:T_range+1)=C_tmp(j,t0:T_range+1)+r(0+1,j,t0)*delta_t*eta(0+1,j,t0);
-             %从unsatisfied_j中去除
-            [~,j_index]=ismember(j,unsatisfied_j);
-            unsatisfied_j(j_index)=[];
-            [~,beta_sorted_j_pointer]=sort(beta(1,unsatisfied_j,t0),'descend');
-            satisfied_cnt=satisfied_cnt+1;
+            r0=(C_qos(j)-C_tmp(j,T_range+1))/delta_t;
+%             [~,P0_index]=min(abs(r(0+1,j,t0,:)-r0));
+%             P0=P_s(P0_index);
+%             eta(0+1,j,t0)=P0/P_i_max;
+            eta(0+1,j,t0)=r0/r(0+1,j,t0,101);
+            satisfied(j)=1;
+            C_tmp(j,t0:T_range+1)=C_max(j);
         end
         S=[S;[0,j,t0,0,0,0]];
-        ijt_j_pointer=ijt_j_pointer+1;
-        if isempty(unsatisfied_j)
-            break
+        j=j+1;
+        j_add=j_add+1;
+        if j>J_range
+            j=j-J_range;
         end
-        if ijt_j_pointer-satisfied_cnt>length(unsatisfied_j)
+        if prod(satisfied)
             break
-        else
-            j=unsatisfied_j(beta_sorted_j_pointer(ijt_j_pointer-satisfied_cnt));
         end
     end
-    
 end
-if ~isempty(unsatisfied_j)
-    for j=unsatisfied_j
+if ~prod(satisfied)
+    for j=(1:J_range)
+        if ~satisfied(j)
         %fprintf('ref:用户%d未能满足QoS限制',j)
-        C_max(j)=C_tmp(j,T_range+1);
+            C_max(j)=C_tmp(j,T_range+1);
+        end
     end
 end
 %第一部分结束
--- matlab/srt_algorithm.m
+++ matlab/srt_algorithm.m
@@ -2,28 +2,25 @@ function [E_stage_2,E_stage_3,C_unsatisfied]=srt_algorithm(beta,J_range,T_range,
 % service begin time -- bt
 % service end time   -- et
 P=reshape([P_i_max;ones(J_range,1)*P_j_max]*ones(1,J_range*T_range),J_range+1,J_range,T_range);
-P1_tmp=linspace(0,P_i_max,101);P2_tmp=linspace(0,P_j_max,11);
+P_s=linspace(0,P_i_max,101);
+P_i_tmp=ones(J_range+1,J_range,T_range,1).*reshape(P_s,[1,1,1,101]);
 %fast fading, size(h0,3)>T_range
 T_range_h0=size(h0,3);
 t_step=T_range_h0/T_range;
-r=zeros(J_range+1,J_range,T_range);
+r=zeros(J_range+1,J_range,T_range,101);
 F_x= @(x,z) exp(-z.*x)./x;
-if t_step>=1
+if t_step>1
     for t=(1:T_range)
         t_h0=(t-1)*t_step;
-        r(:,:,t)=sum(BW*log(1+P(:,:,t).*ones(1,1,t_step).*(beta(:,:,t).*ones(1,1,t_step)).*abs(h0(:,:,t_h0+1:t_h0+t_step)).^2/sigma2)/log(2),3);
+        r(:,:,t,:)=sum(BW*log2(1+P_i_tmp(:,:,t,:).*(beta(:,:,t).*ones(1,1,t_step,101)).*(abs(h0(:,:,t_h0+1:t_h0+t_step)).^2.*ones(1,1,1,101))/sigma2),3);
     end
     r=r/t_step;
-%     r_g=r;
 else
-    gamma=beta.*P./sigma2;
+    gamma=beta.*ones(1,1,1,101).*P_i_tmp./sigma2;
     F_int=integral(@(x) F_x(x,1./gamma),1,inf,'ArrayValued',true);
     r=BW*log2(exp(1)).*exp(1./gamma).*F_int;
     r(isnan(r))=0;r(r==inf)=0;
 end
-
-
-% r=BW*log(1+P.*beta.*h0.^2/sigma2)/log(2);
 
 S=[];%[i,j,t]->[0,j,t,0,0,0]
 %以下为算法第一部分
@@ -34,7 +31,7 @@ C_plausible=C_qos;
 %C_tmp(j,t) means have that much data at the time slot before t
 
 for j=(1:J_range) %遍历用户j
-    [~,beta_j_sorted_t_index]=sort(r(1,j,bt(j):et(j)),'descend');
+    [~,beta_j_sorted_t_index]=sort(r(1,j,bt(j):et(j),101),'descend');
     ijt_t_pointer=1;
     while C_tmp(j,T_range+1)<C_plausible(j) %若不满足qos条件，增加一条边
         if isempty(beta_j_sorted_t_index)
@@ -42,17 +39,21 @@ for j=(1:J_range) %遍历用户j
             break
         end
         t0=beta_j_sorted_t_index(ijt_t_pointer)+bt(j)-1;
-        if r(0+1,j,t0)<=0
+        if r(0+1,j,t0,101)<=0
             %fprintf('srt:用户%d未能满足QoS限制',j)
             C_plausible(j)=C_tmp(j,T_range+1);
             break
         end
-        if C_tmp(j,T_range+1)+r(0+1,j,t0)*delta_t<C_qos(j)%未溢出
+        if C_tmp(j,T_range+1)+r(0+1,j,t0,101)*delta_t<C_qos(j)%未溢出
             eta(0+1,j,t0)=1;%index 0 for BS-i
-            C_tmp(j,t0:T_range+1)=C_tmp(j,t0:T_range+1)+r(0+1,j,t0)*delta_t*eta(0+1,j,t0);
+            C_tmp(j,t0:T_range+1)=C_tmp(j,t0:T_range+1)+r(0+1,j,t0,101)*delta_t;
         else%溢出
-            eta(0+1,j,t0)=(C_qos(j)-C_tmp(j,T_range+1))/(r(0+1,j,t0)*delta_t);
-            C_tmp(j,t0:T_range+1)=C_tmp(j,t0:T_range+1)+r(0+1,j,t0)*delta_t*eta(0+1,j,t0);
+            r0=(C_qos(j)-C_tmp(j,T_range+1))/delta_t;
+%             [~,P0_index]=min(abs(r(0+1,j,t0,:)-r0));
+%             P0=P_s(P0_index);
+%             eta(0+1,j,t0)=P0/P_i_max;
+            eta(0+1,j,t0)=r0/r(0+1,j,t0,101);
+            C_tmp(j,t0:T_range+1)=C_plausible(j);
         end
         
         S=[S;[0,j,t0,0,0,0]];
@@ -65,7 +66,7 @@ for j=(1:J_range) %遍历用户j
     
 end
 %第一部分结束
-E_stage_1=sum(sum(eta(1,:,:)))*P_i_max*delta_t;
+E_stage_1=sum(sum(sum(P.*eta*delta_t)));
 %fprintf('第一部分结束，系统总功耗：%d\n',E_stage_1)
 delta_1=reshape(eta(0+1,:,:),J_range,T_range);
 
@@ -132,7 +133,7 @@ while sum(t_OOC)~=0 %超出基站能力范围
     end    
 end 
 %第二部分结束
-E_stage_2=sum(sum(eta(1,:,:)))*P_i_max*delta_t;
+E_stage_2=sum(sum(sum(P.*eta*delta_t)));
 %fprintf('第二部分结束，系统总能耗：%d\n',E_stage_2)
 %reshape(delta(0+1,:,:),J_range,T_range)
 delta_2=reshape(eta(0+1,:,:),J_range,T_range);
@@ -143,7 +144,7 @@ r0_index=find(eta(0+1,:,:)>0);
 [~,jj,tt]=ind2sub(size(eta(0+1,:,:)),r0_index);
 for j=(1:J_range)%for all user j
     R=[];
-    r0_j=r(0+1,j,tt(jj==j));
+    r0_j=r(0+1,j,tt(jj==j),101);
     if isempty(r0_j)
         continue
     end
@@ -151,17 +152,17 @@ for j=(1:J_range)%for all user j
     for j_prime_R=(1:J_range)
         
         t2s=(bt(j):et(j));
-        t2s=t2s(r(j_prime_R+1,j,t2s)>r0_max);
+        t2s=t2s(r(j_prime_R+1,j,t2s,11)>r0_max);
         for t2=t2s
             %disp([j_prime_R j sum(delta(:,j_prime_R,t2)>0)+sum(delta(j_prime_R+1,:,t2)>0)-(delta(j_prime_R+1,j,t2)>0) sum(delta(:,j,t2)>0)-(delta(j_prime_R+1,j,t2)>0)+sum(delta(j+1,:,t2)>0) t2])
             if j_prime_R~=j && sum(eta(:,j_prime_R,t2)>0)+sum(eta(j_prime_R+1,:,t2)>0)-2*(eta(j_prime_R+1,j,t2)>0)==0 && sum(eta(:,j,t2)>0)-2*(eta(j_prime_R+1,j,t2)>0)+sum(eta(j+1,:,t2)>0)==0 && sum(sum(eta(:,:,t2)>0))-(eta(j_prime_R+1,j,t2)>0)<N && eta(j_prime_R+1,j,t2)<1% j' and j and sys free
                 
                 t1s=(bt(j):et(j));
-                t1s=t1s(r(0+1,j_prime_R,t1s)>r0_max);
+                t1s=t1s(r(0+1,j_prime_R,t1s,101)>r0_max);
                 for t1=t1s
                     if t1~=t2 && sum(eta(:,j_prime_R,t1)>0)-2*(eta(0+1,j_prime_R,t1)>0)+sum(eta(j_prime_R+1,:,t1)>0)==0 && sum(sum(eta(:,:,t1)>0))-(eta(0+1,j_prime_R,t1)>0)<N && eta(0+1,j_prime_R,t1)<1 % j' free and sys availible
-                        r1=r(1,j_prime_R,t1);%r1
-                        if ((t1>t2 && C_tmp(j_prime_R,t2)>=r0_max*delta_t) || (t1<=t2 && C_tmp(j_prime_R,t2)+r1*delta_t>=r0_max*delta_t))
+                        r1_max=r(0+1,j_prime_R,t1,101);%r1
+                        if ((t1>t2 && C_tmp(j_prime_R,t2)>=r0_max*delta_t) || (t1<=t2 && C_tmp(j_prime_R,t2)+r1_max*delta_t>=r0_max*delta_t))
                               R=[R;[0,j_prime_R,t1,j_prime_R,j,t2]];
                         end
                     end
@@ -171,70 +172,47 @@ for j=(1:J_range)%for all user j
         
     end
     while ~isempty(R)
-        r0_index=find(eta(0+1,:,:));% 更新可选r0对应数据
-        [~,jj0,tt0]=ind2sub(size(eta(0+1,:,:)),r0_index);%对应r0:(0+1,j,t0)
-%         r0_j=r(0+1,j,tt0(jj0==j));
-        delta_P=zeros(length(tt0(jj0==j))*size(R,1),3);edges=zeros(length(tt0(jj0==j))*size(R,1),9);
-        for i_0=(1:length(tt0(jj0==j)))
-            ttt=tt0(jj0==j);t0=ttt(i_0);
+        t0s=find(eta(0+1,j,:));% 更新可选r0对应数据
+        delta_E=zeros(length(t0s)*size(R,1),3);edges=zeros(length(t0s)*size(R,1),9);
+        for i_0=(1:length(t0s))
+            t0=t0s(i_0);
             edge0=[0,j,t0];
-            if t_step>=1
-                r0=sum(BW*log(1+P(0+1,j,t0).*ones(1,1,t_step).*(eta(0+1,j,t0).*ones(1,1,t_step)).*(beta(0+1,j,t0).*ones(1,1,t_step)).*abs(h0(0+1,j,(t0-1)*t_step+1:t0*t_step)).^2/sigma2)/log(2),3)/t_step;
-            else
-                gamma=beta(0+1,j,t0)*P(0+1,j,t0)*eta(0+1,j,t0)/sigma2;
-                F_int=integral(@(x) F_x(x,1/gamma),1,inf,'ArrayValued',true);
-                r0=BW*log2(exp(1)).*exp(1/gamma).*F_int;
-            end
+%             r0=r(0+1,j,t0,round(eta(0+1,j,t0)*100+1));
+%             P0=P_i_max*eta(0+1,j,t0);
+            r0=r(0+1,j,t0,101);
+            P0=P_i_max*eta(0+1,j,t0);
 
-            
-%             r0_prime=r(0+1,j,t0)*eta(0+1,j,t0);
             for i_12=(1:size(R,1))
                 edge1=R(i_12,1:3);edge2=R(i_12,4:6);
                 j_prime=edge1(2);
                 t1=edge1(3);t2=edge2(3);
-                beta1=beta(0+1,j_prime,t1);beta2=beta(j_prime+1,j,t2);
-                
-                    if t_step>=1
-                        r1_tmp=sum(BW*log(1+P1_tmp.*ones(1,1,t_step).*(beta1.*ones(1,length(P1_tmp),t_step)).*(abs(h0(0+1,j_prime,(t1-1)*t_step+1:t1*t_step).*ones(1,length(P1_tmp),t_step))).^2/sigma2)/log(2),3)/t_step;
-                        r2_tmp=sum(BW*log(1+P2_tmp.*ones(1,1,t_step).*(beta2.*ones(1,length(P2_tmp),t_step)).*(abs(h0(j_prime+1,j,(t2-1)*t_step+1:t2*t_step).*ones(1,length(P2_tmp),t_step))).^2/sigma2)/log(2),3)/t_step;
-                    else
-                        gamma1=beta1.*P1_tmp/sigma2;
-                        F_int=integral(@(x) F_x(x,1./gamma1),1,inf,'ArrayValued',true);
-                        r1_tmp=BW*log2(exp(1)).*exp(1./gamma1).*F_int;
-                        r1_tmp(isnan(r1_tmp))=0;
-                        gamma2=beta2.*P2_tmp/sigma2;
-                        F_int=integral(@(x) F_x(x,1./gamma2),1,inf,'ArrayValued',true);
-                        r2_tmp=BW*log2(exp(1)).*exp(1./gamma2).*F_int;
-                        r2_tmp(isnan(r2_tmp))=0;
-                    end
-                    
-                    [~,P1_index]=min(abs(r1_tmp-r0));
-                    P1=P1_tmp(P1_index);
-                    [~,P2_index]=min(abs(r2_tmp-r0));
-                    P2=P2_tmp(P2_index);
-%                     [~,P1_index_g]=min(abs(r1_tmp_g-r0));
-%                     [~,P2_index_g]=min(abs(r2_tmp_g-r0));
-%                 if abs(P1_index_g-P1_index)>20 ||abs( P2_index_g-P2_index)>2
-%                     [0,j,t0]
-%                 end
-                
+%                 [~,P1_index]=min(abs(r(0+1,j_prime,t1,:)-r0));
+%                 P1=P_s(P1_index);
+%                 eta1=P1/P_i_max;
+%                 [~,P2_index]=min(abs(r(j_prime+1,j,t2,:)-r0));
+%                 P2=P_s(P2_index);
+%                 eta2=P2/P_i_max;
+                eta1=r0*eta(0+1,j,t0)/r(0+1,j_prime,t1,101);
+                eta2=r0*eta(0+1,j,t0)/r(j_prime+1,j,t2,11);
+                P1=P_i_max*eta1;P2=P_j_max*eta2;
                 ii=(i_0-1)*size(R,1)+i_12;
-                if eta(0+1,j_prime,t1)+P1/P_i_max<1 && eta(j_prime+1,j,t2)+P2/P_j_max<1 && sum(eta(:,j_prime,t1)>0)-2*(eta(0+1,j_prime,t1)>0)+sum(eta(j_prime+1,:,t1)>0)==0 && sum(eta(:,j_prime,t2)>0)+sum(eta(j_prime+1,:,t2)>0)-2*(eta(j_prime+1,j,t2)>0)==0 && sum(eta(j+1,:,t2)>0)+sum(eta(:,j,t2)>0)-2*(eta(j_prime+1,j,t2)>0)==0 && sum(sum(eta(:,:,t2)>0))-(eta(j_prime+1,j,t2)>0)<N && sum(sum(eta(:,:,t1)>0))-(eta(0+1,j_prime,t1)>0)<N
-                    delta_P(ii,:)=[P1,P2,P_i_max-(P1+P2)];
+                if eta(0+1,j_prime,t1)+eta1<1 && eta(j_prime+1,j,t2)+eta2<1 && sum(eta(:,j_prime,t1)>0)-2*(eta(0+1,j_prime,t1)>0)+sum(eta(j_prime+1,:,t1)>0)==0 && sum(eta(:,j_prime,t2)>0)+sum(eta(j_prime+1,:,t2)>0)-2*(eta(j_prime+1,j,t2)>0)==0 && sum(eta(j+1,:,t2)>0)+sum(eta(:,j,t2)>0)-2*(eta(j_prime+1,j,t2)>0)==0 && sum(sum(eta(:,:,t2)>0))-(eta(j_prime+1,j,t2)>0)<N && sum(sum(eta(:,:,t1)>0))-(eta(0+1,j_prime,t1)>0)<N
+                    delta_E(ii,:)=[eta1,eta2,(P0-(P1+P2))*delta_t];
                 else
-                    delta_P(ii,:)=[P1,P2,-inf];
+                    delta_E(ii,:)=[eta1,eta2,-inf];
                 end
                 edges(ii,:)=[edge0,edge1,edge2];
             end
         end
-        [m,ii]=max(delta_P(:,3));%得到最优的替换方式 delta_P(ii,:)=[P1,P2,P_i_max-(P1+P2)];
+    
+        [m,ii]=max(delta_E(:,3));%得到最优的替换方式 delta_P(ii,:)=[P1,P2,P_i_max-(P1+P2)];
         if m>0
             %替换
             edge0=edges(ii,1:3);edge1=edges(ii,4:6);edge2=edges(ii,7:9);
             j_prime=edge1(2);
             t0=edge0(3);t1=edge1(3);t2=edge2(3);
-            r0=r(0+1,j,t0);
-            P1=delta_P(ii,1);P2=delta_P(ii,2);
+            r0=r(0+1,j,t0,round(eta(0+1,j,t0)*100+1));
+            eta1=delta_E(ii,1);eta2=delta_E(ii,2);
             %去除edge0边.
             eta(0+1,j,t0)=0;
             [~,S_index]=ismember([0,j,t0,0,0,0],S,'rows');
@@ -246,11 +224,11 @@ for j=(1:J_range)%for all user j
             C_tmp(j,t0:T_range+1)=C_tmp(j,t0:T_range+1)-r0*delta_t;
             %fprintf('去掉一条边：[0 %d %d]\n',j,t0)
             %加入edge2边
-            eta(j_prime+1,j,t2)=eta(j_prime+1,j,t2)+P2/P_j_max;
+            eta(j_prime+1,j,t2)=eta(j_prime+1,j,t2)+eta2;
             C_tmp(j,t2:T_range+1)=C_tmp(j,t2:T_range+1)+r0*delta_t;
             C_tmp(j_prime,t2:T_range+1)=C_tmp(j_prime,t2:T_range+1)-r0*delta_t;%*(1-gamma(j_prime,j))
             %加入edge1边
-            eta(0+1,j_prime,t1)=eta(0+1,j_prime,t1)+P1/P_i_max;
+            eta(0+1,j_prime,t1)=eta(0+1,j_prime,t1)+eta1;
             S=[S;[0,j_prime,t1,j_prime,j,t2]];
             C_tmp(j_prime,t1:T_range+1)=C_tmp(j_prime,t1:T_range+1)+r0*delta_t;%*(1-gamma(j_prime,j))
             %fprintf('增加两条边：[0 %d %d] [%d %d %d]\n',j_prime,t1,j_prime,j,t2)
@@ -259,7 +237,7 @@ for j=(1:J_range)%for all user j
             %[~,R_index]=ismember([0,j_prime,t1,j_prime,j,t2],R,'rows');
             tmp_max=size(R,1);tmp=1;
             while tmp<tmp_max
-                if R(tmp,6)==t2
+                if R(tmp,6)==t2 || ( R(tmp,2)==j_prime && R(tmp,3)==t1 )
                     R(tmp,:)=[];
                     tmp=tmp-1;
                     tmp_max=tmp_max-1;
@@ -269,9 +247,10 @@ for j=(1:J_range)%for all user j
         else
             break
         end
+    
     end
 end
-E_stage_3=sum(sum(sum(eta,3),2).*[P_i_max;ones(J_range,1)*P_j_max]*delta_t);
+E_stage_3=sum(sum(sum(P.*eta*delta_t)));
 %fprintf('第三部分结束，系统总能耗：%d\n',E_stage_3)
 
 end    
